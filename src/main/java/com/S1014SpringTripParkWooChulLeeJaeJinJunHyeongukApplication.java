0c6a53fef0866f171c869c08701edd19b2f9415f
박우철 <woochul102@gmail.com>
2023-11-03 09:12:45 +0900

    first commit

---
+++ src/main/java/com/S1014SpringTripParkWooChulLeeJaeJinJunHyeongukApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S1014SpringTripParkWooChulLeeJaeJinJunHyeongukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S1014SpringTripParkWooChulLeeJaeJinJunHyeongukApplication.class, args);
+	}
+
+}
