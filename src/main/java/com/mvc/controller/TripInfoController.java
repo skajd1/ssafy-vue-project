a0af7ec750027b193e2f6bfbcde76ead529b1d04
jaejin <ghkfuddks@naver.com>
2023-11-03 15:41:21 +0900

    swagger변경

--- src/main/java/com/mvc/controller/TripInfoController.java
+++ src/main/java/com/mvc/controller/TripInfoController.java
@@ -22,27 +22,27 @@ public class TripInfoController {
 	@Autowired
 	TripInfoService service;
 	
-	@ApiOperation(value="모든 여행지 정보를 반환한다",response=List.class)
+	@ApiOperation(value="전체 관광지 리스트",notes="모든 여행지 정보를 반환한다")
 	@GetMapping("/all")
 	public List<TripInfo> getInfoAll(){
 		
 		return service.getInfoAll();
 	}
 	
-	@ApiOperation(value="컨텐트 타입 별 관광지 정보 반환",response=List.class)
+	@ApiOperation(value="컨텐트 타입 별 관광지 리스트",notes="컨텐트 타입 별 관광지 정보를 반환한다")
 	@GetMapping("/contentTypeId/{contentTypeId}")
 	public List<TripInfo> searchContentId(@PathVariable("contentTypeId") String contentTypeId){
 		
 		return service.searchContentTypeId(contentTypeId);
 	}
 	
-	@ApiOperation(value="지역 코드 별 관광지 정보 반환",response=List.class)
+	@ApiOperation(value="지역 코드 별 관광지 리스트",notes="지역 코드 별 관광지 정보를 반환한다")
 	@GetMapping("/area/{sidocode}/{guguncode}")
 	public List<TripInfo> searchArea(@PathVariable("sidocode") String sidocode, @PathVariable("guguncode") String guguncode){
 		return service.searchArea(Integer.parseInt(sidocode),Integer.parseInt(guguncode));
 	}
 	
-	@ApiOperation(value="관광지 명 별 관광지 정보 반환",response=List.class)
+	@ApiOperation(value="관광지명 별 관광지 리스트",notes="관광지명으로 검색한 관광지 정보를 반환한다 ")
 	@GetMapping("/title/{word}")
 	public List<TripInfo> searchTitle(@PathVariable("word") String word){
 		return service.searchTitle(word);
